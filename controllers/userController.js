88a5513cfc299699c1c43eb031eb8c8161854606
wissemsamtouni <91263921+wissemsamtouni@users.noreply.github.com>
2023-02-27 12:19:51 +0100

    Update userController.js

--- controllers/userController.js
+++ controllers/userController.js
@@ -6,7 +6,7 @@ const crypto = require("crypto");
 const sendEmail = require("../utils/sendEmail");
 const config = {
     user: 'serveur', // better stored in an app setting such as process.env.DB_USER
-    password: 'Wissemsm@', // better stored in an app setting such as process.env.DB_PASSWORD
+    password: '', // better stored in an app setting such as process.env.DB_PASSWORD
     server: 'sqlpfeserveur.database.windows.net', // better stored in an app setting such as process.env.DB_SERVER
     port: 1433, // optional, defaults to 1433, better stored in an app setting such as process.env.DB_PORT
     database: 'pfedb', // better stored in an app setting such as process.env.DB_NAME
